41360ea49e38d02234bf9808f3000ee36bff5792
LorenzoCrespan <lorenzo.crespan@gmail.com>
2022-08-03 11:52:41 +0200

    Comments

--- client/src/main.js
+++ client/src/main.js
@@ -1,5 +1,20 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import '../src/assets/tailwind.css'
-createApp(App).use(router).mount('#app')
+// Documentazione: https://vuejs.org
+// Documentazione: https://router.vuejs.org
+// Documentazione: https://tailwindcss.com
+
+// Le applicazione basate su Vue pongono le basi sulla creazione di una "application instance"
+// prodotta dalla funzione "createApp".
+import { createApp } from "vue";
+// La funzione "createApp" necessita della root component dell'applicazione.
+// Una component, Single-File Components (avente estensione .vue), è un file che permette di incapsulare
+// il codice (HTML, CSS, JS, VUE) necessario alla creazione di un elemento dell'applicazione.
+import App from "./App.vue";
+// La libreria Vue Router permette di mappare le diverse componenti in modo tale da creare una gerarchia
+// all'interno dell'applicazione.
+import router from "./router";
+// Import nell'applicazione della libreria CSS di Tailwind.
+import "../src/assets/tailwind.css";
+
+// Creazione e caricamento dell'applicazione, dando come root component "App".
+// "use(router)" permette all'applicazione di essere router-aware.
+createApp(App).use(router).mount("#app");
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,8 +1,17 @@
+// Import delle funzioni per la creazione di una Vue Router.
+// createRouter: permette di creare un'istanza Router utilizzabile dall'applicazione Vue.
+// createWebHistory: Crea una cronologia HTML5. L'applicazione deve essere servita tramite il protocollo http.
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+// Import delle Single-File Components da utilizzare come riferimento per la Vue Router.
 import LoginView from "../views/LoginView.vue";
+import HomeView from "../views/HomeView.vue";
 import HTTP404ErrorView from "../views/HTTP_Error/HTTP404ErrorView.vue";
 
+// Creazione di una routes per l'applicazione.
+// In ogni singola componente sono identificati i seguenti parametri:
+//		1. path 
+//		2. name
+//		3. component
 const routes = [
 	{
 		path: "/",
