82301633b2f2f0f57f64bf76f9ba8355d0ae6bcb
lorenzocrespan <lorenzo.crespan@gmail.com>
2022-11-08 23:26:41 +0100

    Init login POST Request

--- client/src/components/LoginModal.vue
+++ client/src/components/LoginModal.vue
@@ -1,89 +1,138 @@
 <template>
-    <div class="relative z-[4]">
-        <div class="fixed inset-0 bg-black/50 backdrop-blur-sm transition-opacity"></div>
-        <div class="fixed z-[4] inset-0 overflow-y-auto">
-            <div class="flex items-end sm:items-center justify-center min-h-full text-center sm:p-0">
-                <div
-                    class=" bg-white rounded-lg text-left overflow-hidden shadow-xl transform sm:my-8 sm:max-w-lg sm:w-full">
-                    <div class="px-4 pt-5 pb-5">
-                        <div>
-                            <img class="mx-auto w-[120px] h-auto" src="../../src/assets/images/black_kiwi.svg"
-                                alt="Workflow">
-                            <h2 class="mt-6 text-center text-3xl font-extrabold text-slate-900">
-                                Accedi al tuo account
-                            </h2>
-                        </div>
+	<div class="relative z-[4]">
+		<div
+			class="fixed inset-0 bg-black/50 backdrop-blur-sm transition-opacity"
+		></div>
+		<div class="fixed z-[4] inset-0 overflow-y-auto">
+			<div
+				class="flex items-end sm:items-center justify-center min-h-full text-center sm:p-0"
+			>
+				<div
+					class="bg-white rounded-lg text-left overflow-hidden shadow-xl transform sm:my-8 sm:max-w-lg sm:w-full"
+				>
+					<div class="px-4 pt-5 pb-5">
+						<div>
+							<img
+								class="mx-auto w-[120px] h-auto"
+								src="../../src/assets/images/black_kiwi.svg"
+								alt="Workflow"
+							/>
+							<h2
+								class="mt-6 text-center text-3xl font-extrabold text-slate-900"
+							>
+								Accedi al tuo account
+							</h2>
+						</div>
 
-                        <form class="mt-8 space-y-6" @submit.prevent="onSubmit">
-                            <input type="hidden" name="remember" value="true">
-                            <div class="rounded-md shadow-sm -space-y-px">
-                                <div>
-                                    <label for="email-address" class="sr-only">
-                                        Indirizzo Email
-                                    </label>
-                                    <input id="email-address" name="email" type="email" autocomplete="email" required
-                                        class="appearance-none rounded-none relative block w-full px-3 py-2 border border-slate-300 placeholder-slate-500 text-slate-900 rounded-t-md focus:outline-none focus:ring-slate-600 focus:border-slate-600 focus:z-[8] sm:text-sm"
-                                        placeholder="Email address" v-model="formData.mail">
-                                </div>
-                                <div>
-                                    <label for="password" class="sr-only">
-                                        Password
-                                    </label>
-                                    <input id="password" name="password" type="password" autocomplete="current-password"
-                                        required
-                                        class="appearance-none rounded-none relative block w-full px-3 py-2 border border-slate-300 placeholder-slate-500 text-slate-900 rounded-b-md focus:outline-none focus:ring-slate-600 focus:border-slate-600 focus:z-[8] sm:text-sm"
-                                        placeholder="Password" v-model="formData.password">
-                                </div>
-                            </div>
+						<form class="mt-8 space-y-6" @submit.prevent="onSubmit">
+							<input type="hidden" name="remember" value="true" />
+							<div class="rounded-md shadow-sm -space-y-px">
+								<div>
+									<label for="email-address" class="sr-only">
+										Indirizzo Email
+									</label>
+									<input
+										id="email-address"
+										name="email"
+										type="email"
+										autocomplete="email"
+										required
+										class="appearance-none rounded-none relative block w-full px-3 py-2 border border-slate-300 placeholder-slate-500 text-slate-900 rounded-t-md focus:outline-none focus:ring-slate-600 focus:border-slate-600 focus:z-[8] sm:text-sm"
+										placeholder="Email address"
+										v-model="formData.mail"
+									/>
+								</div>
+								<div>
+									<label for="password" class="sr-only"> Password </label>
+									<input
+										id="password"
+										name="password"
+										type="password"
+										autocomplete="current-password"
+										required
+										class="appearance-none rounded-none relative block w-full px-3 py-2 border border-slate-300 placeholder-slate-500 text-slate-900 rounded-b-md focus:outline-none focus:ring-slate-600 focus:border-slate-600 focus:z-[8] sm:text-sm"
+										placeholder="Password"
+										v-model="formData.password"
+									/>
+								</div>
+							</div>
 
-                            <div class="flex items-center justify-between">
-                                <div class="flex items-center">
-                                    <input id="remember-me" name="remember-me" type="checkbox"
-                                        class="h-4 w-4 text-slate-600 border-slate-600 rounded">
-                                    <label for="remember-me" class="ml-2 block text-sm text-slate-600">
-                                        Ricordami
-                                    </label>
-                                </div>
+							<div class="flex items-center justify-between">
+								<div class="flex items-center">
+									<input
+										id="remember-me"
+										name="remember-me"
+										type="checkbox"
+										class="h-4 w-4 text-slate-600 border-slate-600 rounded"
+									/>
+									<label
+										for="remember-me"
+										class="ml-2 block text-sm text-slate-600"
+									>
+										Ricordami
+									</label>
+								</div>
 
-                                <div class="text-sm">
-                                    <a href="404"
-                                        class="font-medium text-slate-600 hover:text-slate-900 hover:line-through">
-                                        Hai dimenticato la password?
-                                    </a>
-                                </div>
-                            </div>
+								<div class="text-sm">
+									<a
+										href="404"
+										class="font-medium text-slate-600 hover:text-slate-900 hover:line-through"
+									>
+										Hai dimenticato la password?
+									</a>
+								</div>
+							</div>
 
-                            <div>
-                                <button type="submit"
-                                    class="relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md bg-slate-300 text-slate-900  hover:bg-slate-900 hover:text-white">
-                                    Accedi
-                                </button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+							<div>
+								<button
+									type="submit"
+									class="relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md bg-slate-300 text-slate-900 hover:bg-slate-900 hover:text-white"
+								>
+									Accedi
+								</button>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
 export default {
-    props: [],
-    data() {
-        return {
-            formData: {
-                mail: '',
-                password: ''
-            },
-        }
-    },
-    methods: {
-        onSubmit() {
-            console.log("Submitted form")
-            console.log(this.formData)
-            this.$router.push("home")
-        }
-    }
-}
+	props: [],
+	data() {
+		return {
+			formData: {
+				mail: "",
+				password: "",
+			},
+		};
+	},
+	methods: {
+		onSubmit() {
+			console.log("Submitted form");
+
+			var formdata = new FormData();
+			formdata.append("username", this.formData.mail);
+			formdata.append("password", this.formData.password);
+
+			var requestOptions = {
+				method: "POST",
+				body: formdata,
+			};
+
+			fetch("http://localhost:8081/login", requestOptions)
+				.then((response) => {
+					console.log(response);
+					if (response.status == 200) {
+						this.$router.push("home");
+						// TODO: Gestione errori
+					}
+				})
+				.catch((error) => console.log("error", error));
+		},
+	},
+};
 </script>
